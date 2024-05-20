2a777c5b9253e79ec47f6354206c822dc72111f8
hanacoric <hancor01@easv365.dk>
2024-05-21 00:12:33 +0200

    coded mobile version for single page product

--- components/productDetails.vue
+++ components/productDetails.vue
@@ -1,17 +1,18 @@
 <template>
- <div v-if="product" class="w-full h-[120vh]">
-  <h1 class="h2-pink mb-10 ml-[160px]">{{ product.name }}</h1>
+ <div v-if="product" class="w-full h-max">
+    <div class="w-full flex flex-row lg:justify-start justify-center" ><h1 class="lg:h2-pink mb-10 lg:ml-[160px] h2-brown ">{{ product.name }}</h1></div>
+  
   <div >
     <div class="flex justify-around ">
         <div class="max-w-2xl flex justify-start items-center flex-col">
-            <img :src="product.images[0].src" alt="Product Image" class="w-[488px] h-[488px] rounded-lg border-2" style="border-color: #F0B9AC;">
+            <img :src="product.images[0].src" alt="Product Image" class="lg:w-[488px] lg:h-[488px] sm:w-[188px] sm:h-[188px] rounded-lg border-2 " style="border-color: #F0B9AC;">
             <div class="text-left flex justify-center items-center flex-col">
-                <h2 class="w-[75%] h3-pink flex justify-start ">Product description:</h2>
+                <h2 class="lg:w-[75%] lg:h3-pink lg:flex lg:justify-start hidden ">Product description:</h2>
                 <div v-html="product.description" class=" description-wrapper"></div>
-                <div v-html="product.short_description" class="w-[75%] description-wrapper m-0"></div>
+                <div v-html="product.short_description" class="w-[75%] description-wrapper m-0 lg:block hidden "></div>
             </div>
         </div>
-        <div class="w-full flex flex-col p-8 rounded-lg border-2 max-w-2xl fill-white" style="border-color: #F0B9AC;">
+        <div class="w-full flex flex-col p-8 rounded-lg border-2 max-w-2xl fill-white lg:block hidden" style="border-color: #F0B9AC;">
     <div>
     <div class="flex items-center justify-between ">
       <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
@@ -170,10 +171,75 @@
 </div>
     </div>
 </div>
+<!--mobile version-->
+<div class="w-full h-fit flex flex-col justify-center items-center lg:hidden" >
+<div class="w-full flex flex-col justify-center items-center">
+<div class="w-full flex justify-start mt-10">
+    <p class=" w-fit h-[50px] h2-brown lg:hidden flex m-0">Slices:</p>
 </div>
+<div class="w-fit flex flex-row justify-center space-x-4 items-center">
+    <button class="btn-product hover-pink">8</button>
+    <button class="btn-product hover-pink">14</button>
+    <button class="btn-product hover-pink">20</button>
+</div>
+<div class="w-fit flex flex-col justify-center items-center">
+ <div class="w-full flex justify-start mt-10">
+    <p class=" h-[50px] h2-brown lg:hidden flex m-0 ">Style:</p>
+</div>
+<div class="w-fit flex flex-row justify-center space-x-4 items-center">
+    <button class="btn-product hover-pink">Traditional</button>
+    <button class="btn-product hover-pink">Gluten-free</button>
+    <button class="btn-product hover-pink">Vegan</button>
+</div>
+</div>
+</div>
+<div class=" w-full flex flex-col justify-between mt-10">
+    <div class="flex flex-row justify-evenly space-x-20">
+        <div class="flex flex-row">
+            <input type="checkbox" class="focus:ring-0 before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10">
+        <p class="text-[#191514] font-['New_Spirit'] text-xl mt-0 ml-3">Box:</p>
+        </div>
+        <p class="text-[#191514] font-['New_Spirit'] text-xl m-0 flex flex-row ">+ 500 HUF</p>
+    </div>
+    <div class="flex flex-row justify-evenly space-x-20 mt-5">
+        <p class="text-[#191514] font-['New_Spirit'] text-xl mt-0 ml-3 font-bold">Sub-total:</p>
+        <p class="text-[#191514] font-['New_Spirit'] text-xl m-0 flex flex-row ">8 250 HUF</p>
+    </div>
+</div>
+    <div class="flex justify-center">
+    <button class=" w-[200px] h-[50px] p-product-black text-lg font-bold py-2 rounded-lg" style="background-color: #F0B9AC;">
+        Add to cart
+    </button>
+</div>
+
+<div class="w-full flex flex-col justify-between mt-5">
+    <div class="w-full flex justify-center mt-10">
+    <p class=" w-fit h-[50px] h2-brown lg:hidden flex m-0">Ingredients:</p>
+</div>
+<div class="w-fit flex flex-row justify-center space-x-4 items-center">
+    <p class="text-lg m-0 p-product-black">
+                <a href="#" class="underline">flour</a> - 
+                <a href="#" class="underline ">egg</a> - 
+                sugar - 
+                <a href="#" class="underline ">almond flour</a> - 
+                margarine - sugar - apple - cinnamon - white chocolate - baking powder - 
+                <a href="#" class="underline text-brown-600 hover:text-brown-800">cream</a>
+            </p>
+</div>
+<div class="w-full flex justify-center mt-10">
+    <p class=" w-fit h-[50px] h2-brown lg:hidden flex m-0">Product Description:</p>
+</div>
+    <div v-html="product.short_description" class="description-wrapper-mobile w-[75%]  m-0 lg:hidden"></div>
+</div>
+</div>
+</div>
+
   <div v-else>
     <p>Loading...</p>
   </div>
+
+
+
   
 </template>
 
@@ -210,6 +276,14 @@ the description p tag to be unreachable by tailwind so we have to use some basic
 .description-wrapper p {
     color: hsl(12, 11%, 9%);
     font-size: 1.5rem;
+}
+
+.description-wrapper-mobile p{
+    color: hsl(12, 11%, 9%);
+    font-size: 1rem;
+    font-weight: 400;
+    margin: 0;
+    width: 95vw;
 }
 
 </style>
