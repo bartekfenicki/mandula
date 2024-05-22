1d5baa4eaa0c047f75e241b55c28eb2f44cf107a
hanacoric <hancor01@easv365.dk>
2024-05-22 11:55:53 +0200

    added additional radio inputs

--- components/productDetails.vue
+++ components/productDetails.vue
@@ -8,8 +8,7 @@
             <img :src="product.images[0].src" alt="Product Image" class="lg:w-[488px] lg:h-[488px] sm:w-[188px] sm:h-[188px] rounded-lg border-2 " style="border-color: #F0B9AC;">
             <div class="text-left flex justify-center items-center flex-col">
                 <h2 class="lg:w-[75%] lg:h3-pink lg:flex lg:justify-start hidden ">Product description:</h2>
-                <div v-html="product.description" class=" description-wrapper"></div>
-                <div v-html="product.short_description" class="w-[75%] description-wrapper m-0 lg:block hidden "></div>
+                <div v-html="product.short_description" class="w-[75%] short-description-wrapper m-0 lg:block hidden "></div>
             </div>
         </div>
         <div class="w-full flex flex-col p-8 rounded-lg border-2 max-w-2xl fill-white lg:block hidden" style="border-color: #F0B9AC;">
@@ -26,13 +25,13 @@
     <div class="ml-[40px] flex flex-col justify-center items-start" v-if="showMenu">
     <div class="flex flex-col justify-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+      <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Traditional</label> </label>
         <label class=" flex items-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+        <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Gluten-free</label> </label>
@@ -40,7 +39,7 @@
         </label>
         <label class=" flex items-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+        <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Vegan</label> </label>
@@ -51,11 +50,7 @@
         
         <div class="p-product-black">
             <div class="p-product-black">INGREDIENTS:</div>
-            <a href="#" class="underline">flour</a> -
-            <a href="#" class="underline">egg</a> -
-            sugar - <a href="#" class="underline">almond flour</a> -
-            margarine - sugar - apple - cinnamon - white chocolate - baking powder -
-            <a href="#" class="underline">cream</a>
+            <div v-html="product.description" class="ingredients"></div>
         </div>
     </div>
 </div>
@@ -67,19 +62,19 @@
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink"  /> 
     <label for="default-checkbox" class="p-product mt-0 ml-5">14 Slices</label> </label>
-        <p class="flex items-left p-product mt-0">{{ product.price }}</p>
+        <p class="flex items-left p-product mt-0"></p>
     </div>
 
     <div class="ml-[40px] flex flex-col justify-center items-start" v-if="showMenuTwo">
     <div class="flex flex-col justify-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+        <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Traditional</label> </label>
         <label class=" flex items-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+        <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Gluten-free</label> </label>
@@ -87,7 +82,7 @@
         </label>
         <label class=" flex items-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+    <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Vegan</label> </label>
@@ -118,13 +113,13 @@
     <div class="ml-[40px] flex flex-col justify-center items-start" v-if="showMenuThree">
     <div class="flex flex-col justify-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+    <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Traditional</label> </label>
         <label class=" flex items-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+    <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Gluten-free</label> </label>
@@ -132,7 +127,7 @@
         </label>
         <label class=" flex items-center">
           <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="fill-pink">
-      <input type="checkbox"
+    <input type="radio" name="variations"
         class="focus:ring-0 before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border  checked:border-white-500 checked:fill-pink checked:before:fill-pink hover:before:opacity-10"
         id="fill-pink" checked /> 
     <label for="default-checkbox" class="p-product-black mt-0 ml-5">Vegan</label> </label>
@@ -171,6 +166,9 @@
 </div>
     </div>
 </div>
+
+
+
 <!--mobile version-->
 <div class="w-full h-fit flex flex-col justify-center items-center lg:hidden" >
 <div class="w-full flex flex-col justify-center items-center">
@@ -248,6 +246,8 @@
 const {product} = defineProps(['product'])
 console.log(product)
 
+
+
 const showMenu = ref(true)
 const showMenuTwo = ref(false)
 const showMenuThree = ref(false)
@@ -273,7 +273,7 @@ const ToggleMenu = (id) => {
 <style >
 /*The dinamically added data using v-html caused 
 the description p tag to be unreachable by tailwind so we have to use some basic css to reach it*/
-.description-wrapper p {
+.short-description-wrapper p {
     color: hsl(12, 11%, 9%);
     font-size: 1.5rem;
 }
@@ -284,6 +284,13 @@ the description p tag to be unreachable by tailwind so we have to use some basic
     font-weight: 400;
     margin: 0;
     width: 95vw;
+}
+
+.ingredients p{
+    color: hsl(12, 11%, 9%);
+    font-size: 1rem;
+    margin-top: 0;
+    font-weight: bold;
 }
 
 </style>
